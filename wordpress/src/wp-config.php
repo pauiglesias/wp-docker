ef0f6182ccb5a28fa24813e41cbe749b203a80bb
asimetrica <pauiglesias@gmail.com>
2022-06-27 17:27:21 +0200

    Set the https protocol.

--- wordpress/src/wp-config.php
+++ wordpress/src/wp-config.php
@@ -111,8 +111,8 @@ define( 'WP_DEBUG', !!getenv_docker('WORDPRESS_DEBUG', '') );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
-define('WP_HOME', 'http://wp-docker-test.local');
-define('WP_SITEURL', 'http://wp-docker-test.local');
+define('WP_HOME', 'https://wp-docker-test.local');
+define('WP_SITEURL', 'https://wp-docker-test.local');
 
 // If we're behind a proxy server and using HTTPS, we need to alert WordPress of that fact
 // see also https://wordpress.org/support/article/administration-over-ssl/#using-a-reverse-proxy
